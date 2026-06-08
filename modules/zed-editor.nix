3829eb882754755d90184a6d7d1b3ea333f7fb77
guillaumedsde <30023543+guillaumedsde@users.noreply.github.com>
2026-06-08 22:06:44 +0200

    feat: configure zed

--- modules/zed-editor.nix
+++ modules/zed-editor.nix
@@ -3,8 +3,26 @@
     enable = true;
     extensions = [ "nix" ];
     extraPackages = [
-        pkgs.nil
-        pkgs.nixd
+      pkgs.nil
+      pkgs.nixd
     ];
+    userSettings = {
+      auto_update = false;
+      telemetry = {
+        diagnostics = false;
+        metrics = false;
+      };
+      disable_ai = true;
+      title_bar.show_sign_in = false;
+      collaboration_panel.button = false;
+      outline_panel.button = false;
+      preview_tabs.enabled = false;
+      git_panel.dock = "left";
+      project_panel.dock = "left";
+      base_keymap = "VSCode";
+      # TODO
+      # buffer_font_family = "Fira Code";
+      theme = "Ayu Light";
+    };
   };
 }
