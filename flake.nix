90d8df16beb841561bcf7dc3b083d3939949ab53
guillaumedsde <30023543+guillaumedsde@users.noreply.github.com>
2026-06-21 13:00:42 +0200

    chore: match home-manager version with nixpkgs

--- flake.nix
+++ flake.nix
@@ -5,7 +5,7 @@
     # Specify the source of Home Manager and Nixpkgs.
     nixpkgs.url = "github:nixos/nixpkgs/nixos-26.05";
     home-manager = {
-      url = "github:nix-community/home-manager";
+      url = "github:nix-community/home-manager/release-26.05";
       inputs.nixpkgs.follows = "nixpkgs";
     };
   };
