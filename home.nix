d74fbfb2be7cf5988fd386e0da2fe209834d60bc
guillaumedsde <30023543+guillaumedsde@users.noreply.github.com>
2026-07-25 10:07:28 +0200

    feat: add dev packages

--- home.nix
+++ home.nix
@@ -1,4 +1,4 @@
-{ ... }:
+{ pkgs, ... }:
 
 {
   targets.genericLinux.enable = true;
@@ -21,22 +21,13 @@
   # The home.packages option allows you to install Nix packages into your
   # environment.
   home.packages = [
-    # # Adds the 'hello' command to your environment. It prints a friendly
-    # # "Hello, world!" when run.
-    # pkgs.hello
+    pkgs.talosctl
+    pkgs.kubectl
+    pkgs.argocd
+    pkgs.sops
+    pkgs.opentofu
+    pkgs.jq
 
-    # # It is sometimes useful to fine-tune packages, for example, by applying
-    # # overrides. You can do that directly here, just don't forget the
-    # # parentheses. Maybe you want to install Nerd Fonts with a limited number of
-    # # fonts?
-    # (pkgs.nerdfonts.override { fonts = [ "FantasqueSansMono" ]; })
-
-    # # You can also create simple shell scripts directly inside your
-    # # configuration. For example, this adds a command 'my-hello' to your
-    # # environment:
-    # (pkgs.writeShellScriptBin "my-hello" ''
-    #   echo "Hello, ${config.home.username}!"
-    # '')
   ];
 
   # Home Manager is pretty good at managing dotfiles. The primary way to manage
