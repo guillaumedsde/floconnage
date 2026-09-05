2c0d0d08e04a8f580b871aab62eb206c9ad3df0d
guillaumedsde <30023543+guillaumedsde@users.noreply.github.com>
2026-09-05 09:51:04 +0200

    feat: add virt tools

--- home.nix
+++ home.nix
@@ -27,12 +27,16 @@
     pkgs.sops
     pkgs.opentofu
     pkgs.jq
+    pkgs.virt-manager
     pkgs.ansible-lint
-    (pkgs.python314Packages.toPythonApplication pkgs.python314Packages.ansible-core)
-    (pkgs.python314.withPackages (ps: [
-      ps.libvirt
-      ps.lxml
+    pkgs.cloud-init
+    (pkgs.python3.withPackages (python-pkgs: [
+      python-pkgs.ansible
+      python-pkgs.ansible-core
+      python-pkgs.libvirt
+      python-pkgs.lxml
     ]))
+
   ];
 
   # Home Manager is pretty good at managing dotfiles. The primary way to manage
